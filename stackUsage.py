31432c6d0dec900586b29e2159d0b0de69e045a5
Thanassis Tsiodras <ttsiodras@gmail.com>
2017-07-08 14:08:37 +0200

    Been a while since I posted on my blog.

--- stackUsage.py
+++ stackUsage.py
@@ -1,34 +1,44 @@
-#!/usr/bin/env python
-#
-# Utility to detect recursive calls and calculate total stack usage per function
-# (via following the call graph). Works for x86 and SPARC/Leon binaries.
-#
-# Published under the GPL, (C) 2011 Thanassis Tsiodras
-# Suggestions/comments: ttsiodras@gmail.com
+#!/usr/bin/env python2
+"""
+Utility to detect recursive calls and calculate total stack usage per function
+(via following the call graph). Works for x86 and SPARC/Leon binaries.
 
-import sys, os, re, operator
+Published under the GPL, (C) 2011 Thanassis Tsiodras
+Suggestions/comments: ttsiodras@gmail.com
+"""
 
-# regexp helper
+import os
+import re
+import sys
+import operator
+
+
 class Matcher:
+    """regexp helper"""
     def __init__(self, pattern, flags=0):
         self._pattern = re.compile(pattern, flags)
         self._hit = None
+
     def match(self, line):
         self._hit = re.match(self._pattern, line)
         return self._hit
+
     def search(self, line):
         self._hit = re.search(self._pattern, line)
         return self._hit
+
     def group(self, idx):
         return self._hit.group(idx)
 
-# Detect cycles in function call graphs
+
 def CheckForCycles(callGraph, badNodes):
+    """Detect cycles in function call graphs"""
     def journey(path):
         node = path[-1]
-        if not callGraph.has_key(node):
+        if node not in callGraph:
             return
-        if callGraph[node] == None: # has been marked as recursive - propagate marking
+        if callGraph[node] is None:  # has been marked as recursive,
+                                     # so propagate the marking
             badNodes[:] = path[:]
             return
         neighbours = callGraph[node]
@@ -38,34 +48,42 @@ def CheckForCycles(callGraph, badNodes):
                 return
             journey(path + [neighbour])
     for node, v in callGraph.items():
-        if v: # if not marked as recursive...
+        if v:  # if not marked as recursive...
             journey([node])
             if badNodes:
                 return
 
-# Calculate the total stack usage of each function, taking into account who it calls
-def findStackUsage(foo, stackUsagePerFunction, callGraph, cache={}):
-    if cache.has_key(foo): # memoization
-        return cache[foo]
-    if not stackUsagePerFunction.has_key(foo):
+
+def findStackUsage(fn, stackUsagePerFunction, callGraph, cache={}):
+    """
+    Calculate the total stack usage of each function,
+    taking into account who it calls
+    """
+    #  pylint: disable=W0102
+    if fn in cache:  # memoization
+        return cache[fn]
+    if fn not in stackUsagePerFunction:
         return 0
-    if not callGraph.has_key(foo) or not callGraph[foo]:
-        cache[foo] = stackUsagePerFunction[foo]
-        return stackUsagePerFunction[foo]
-    totalStackUsage = max( # the largest of the possible call chains
-        ((stackUsagePerFunction[foo] + findStackUsage(x, stackUsagePerFunction, callGraph))
-         for x in callGraph[foo]))
-    cache[foo] = totalStackUsage
+    if fn not in callGraph or not callGraph[fn]:
+        cache[fn] = stackUsagePerFunction[fn]
+        return stackUsagePerFunction[fn]
+    totalStackUsage = max(  # the largest of the possible call chains
+        ((stackUsagePerFunction[fn] +
+            findStackUsage(x, stackUsagePerFunction, callGraph))
+         for x in callGraph[fn]))
+    cache[fn] = totalStackUsage
     return totalStackUsage
 
+
 def main():
-    if len(sys.argv)<2 or not os.path.exists(sys.argv[1]):
+    if len(sys.argv) < 2 or not os.path.exists(sys.argv[1]):
         print "Usage: %s ELFbinary" % sys.argv[0]
         sys.exit(1)
     binarySignature = os.popen("file \"%s\"" % sys.argv[1]).readlines()[0]
-    x86  = Matcher(r'ELF 32-bit LSB.*80.86')
+    x86 = Matcher(r'ELF 32-bit LSB.*80.86')
     x64  = Matcher(r'ELF 64-bit LSB.*x86-64')
     leon = Matcher(r'ELF 32-bit MSB.*SPARC')
+    arm = Matcher(r'ELF 32-bit LSB.*ARM')
     if x86.search(binarySignature):
         objdump = 'objdump'
         nm = 'nm'
@@ -83,76 +101,96 @@ def main():
         nm = 'sparc-elf-nm'
         functionNamePattern = Matcher(r'^(\S+) <([a-zA-Z0-9_]+?)>:')
         callPattern = Matcher(r'^.*call\s+\S+\s+<([a-zA-Z0-9_]+)>')
-        stackUsagePattern = Matcher(r'^.*save.*%sp, (-([0-9]{1,})), %sp')
+        stackUsagePattern = Matcher(
+            r'^.*save.*%sp, (-([0-9]{2}|[3-9])[0-9]{2}), %sp')
+    elif arm.search(binarySignature):
+        objdump = 'arm-eabi-objdump'
+        nm = 'arm-eabi-nm'
+        functionNamePattern = Matcher(r'^(\S+) <([a-zA-Z0-9_]+?)>:')
+        callPattern = Matcher(r'^.*bl\s+\S+\s+<([a-zA-Z0-9_]+)>')
+        stackUsagePattern = Matcher(
+            r'^.*sub.*sp, sp, (#[0-9][0-9]*)')
     else:
         print "Unknown signature:", binarySignature
         sys.exit(1)
 
     # Store .text symbol offsets and sizes (use nm)
     offsetOfSymbol = {}
-    for line in os.popen(nm + " \"" + sys.argv[1] + "\" | grep ' [Tt] '").readlines():
-        offset, _, symbol = line.split()
+    for line in os.popen(
+            nm + " \"" + sys.argv[1] + "\" | grep ' [Tt] '").readlines():
+        offset, unused, symbol = line.split()
         offsetOfSymbol[symbol] = int(offset, 16)
     sizeOfSymbol = {}
     lastOffset = 0
     lastSymbol = None
-    for symbol, offset in sorted(offsetOfSymbol.iteritems(), key=operator.itemgetter(1)):
+    for symbol, offset in sorted(
+            offsetOfSymbol.iteritems(), key=operator.itemgetter(1)):
         if lastSymbol:
             sizeOfSymbol[lastSymbol] = offset-lastOffset
         lastSymbol = symbol
         lastOffset = offset
-    sizeOfSymbol[lastSymbol] = 2**31 # allow last .text symbol to roam free
+    sizeOfSymbol[lastSymbol] = 2**31  # allow last .text symbol to roam free
 
     # Parse disassembly to create callgraph (use objdump -d)
-    foundStack = False
     functionName = ""
     stackUsagePerFunction = {}
     callGraph = {}
     insideFunctionBody = False
+    currentFunctionStackSize = 0
 
     offsetPattern = Matcher(r'^([0-9A-Za-z]+):')
     for line in os.popen(objdump + " -d \"" + sys.argv[1] + "\"").readlines():
         # Have we matched a function name yet?
         if functionName != "":
-            # Yes, update "insideFunctionBody" boolean by checking the current offset
-            # against the length of this symbol, stored in sizeOfSymbol[functionName]
+            # Yes, update "insideFunctionBody" boolean by checking
+            # the current offset against the length of this symbol,
+            # stored in sizeOfSymbol[functionName]
             offset = offsetPattern.match(line)
             if offset:
                 offset = int(offset.group(1), 16)
-                if offsetOfSymbol.has_key(functionName):
-                    insideFunctionBody = insideFunctionBody and (offset-offsetOfSymbol[functionName])<sizeOfSymbol[functionName]
+                if functionName in offsetOfSymbol:
+                    startOffset = offsetOfSymbol[functionName]
+                    insideFunctionBody = \
+                        insideFunctionBody and \
+                        (offset - startOffset) < sizeOfSymbol[functionName]
 
         # Check to see if we see a new function:
         # 08048be8 <_functionName>:
-        foo = functionNamePattern.match(line)
-        if foo:
-            offset = int(foo.group(1), 16)
-            functionName = foo.group(2)
+        fn = functionNamePattern.match(line)
+        if fn:
+            offset = int(fn.group(1), 16)
+            functionName = fn.group(2)
             callGraph.setdefault(functionName, set())
             # make sure this is the function we found with nm
-            if offsetOfSymbol.has_key(functionName):
-                if offsetOfSymbol[functionName] != offset:
-                    print "Weird,", functionName, "is not at offset reported by", nm
-                    print hex(offsetOfSymbol[functionName]), hex(offset)
+            # UPDATE: no, can't do - if a symbol is of local file scope
+            # (i.e. if it was declared with 'static')
+            # then it can appear in multiple places...
+            #
+            #if functionName in offsetOfSymbol:
+            #    if offsetOfSymbol[functionName] != offset:
+            #        print "Weird,", functionName, \
+            #            "is not at offset reported by", nm
+            #        print hex(offsetOfSymbol[functionName]), hex(offset)
             insideFunctionBody = True
-            foundStack = False
+            foundFirstCall = False
+            stackUsagePerFunction[functionName] = 0
 
-        # If we're inside a function body (i.e. offset is not out of symbol size range)
+        # If we're inside a function body
+        # (i.e. offset is not out of symbol size range)
         if insideFunctionBody:
             # Check to see if we have a call
-            #  8048c0a:       e8 a1 03 00 00          call   8048fb0 <frame_dummy>
+            #  8048c0a:       e8 a1 03 00 00       call   8048fb0 <frame_dummy>
             call = callPattern.match(line)
             if functionName != "" and call:
+                foundFirstCall = True
                 calledFunction = call.group(1)
                 callGraph[functionName].add(calledFunction)
-            
-            # Check to see if we have the first stack reduction opcode
+
+            # Check to see if we have a stack reduction opcode
             #  8048bec:       83 ec 04                sub    $0x46,%esp
-            if not foundStack and functionName != "":
+            if functionName != "" and not foundFirstCall:
                 stackMatch = stackUsagePattern.match(line)
                 if stackMatch:
-                    # make sure we dont re-update the stackusage for this function
-                    foundStack = True 
                     value = stackMatch.group(1)
                     if value.startswith("0x"):
                         # sub    $0x46,%esp
@@ -161,14 +199,17 @@ def main():
                             # unfortunately, GCC may also write:
                             # add    $0xFFFFFF86,%esp
                             value = 4294967296-value
+                    elif value.startswith("#"):
+                        # sub sp, sp, #1024
+                        value = int(value[1:])
                     else:
                         # save  %sp, -104, %sp
                         value = -int(value)
-                    stackUsagePerFunction[functionName] = value
+                    stackUsagePerFunction[functionName] += value
 
-    #for foo,v in stackUsagePerFunction.items():
-    #   print foo,v
-    #   print "CALLS:", callGraph[foo]
+    #for fn,v in stackUsagePerFunction.items():
+    #   print fn,v
+    #   print "CALLS:", callGraph[fn]
 
     # First, detect cycles and remove "bad" nodes from calculations
     # (recursive calls would lead to infinite stack usage)
@@ -180,21 +221,23 @@ def main():
         lastStep = badNodes[-1] + " (recursive)"
         badNodes.pop()
         badNodes.append(lastStep)
-        print "Detected cycle and will ignore these functions:\n\t", "\n\t".join(badNodes)
+        print "Detected cycle and will ignore these functions:\n\t", \
+              "\n\t".join(badNodes)
         for n in set(badNodes):
-            stackUsagePerFunction[n] = None     #  marked as recursive
-            callGraph[n] = None                 #  marked as recursive
+            stackUsagePerFunction[n] = None     # marked as recursive
+            callGraph[n] = None                 # marked as recursive
 
     print "Cumulative stack usage per function:"
     # Then, navigate the graph to calculate stack needs per function
     results = []
-    for foo, value in stackUsagePerFunction.items():
-        if value != None:
-            results.append((foo, findStackUsage(foo, stackUsagePerFunction, callGraph)))
+    for fn, value in stackUsagePerFunction.items():
+        if value is not None:
+            results.append(
+                (fn, findStackUsage(fn, stackUsagePerFunction, callGraph)))
         #else:
-        #    results.append((foo, 'recursive'))
-    for foo, value in sorted(results, key=operator.itemgetter(1)):
-        print "%10s: %s" % (value, foo)
-    
+        #    results.append((fn, 'recursive'))
+    for fn, value in sorted(results, key=operator.itemgetter(1)):
+        print "%10s: %s" % (value, fn)
+
 if __name__ == "__main__":
     main()
